5665d87fd135ddb0741c2791cd1f24d2f834b600
doctork4 <doctor.k1994@gmail.com>
2023-11-13 15:50:16 +0900

    chore: lint, formatter 설정 추가

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ['airbnb', 'plugin:prettier/recommended'],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'max-depth': ['error', 2],
+    'prefer-const': 2,
+    'no-var': 2,
+    'no-undef': 2,
+    'no-unused-vars': 1,
+    'max-lines-per-function': ['error', 15],
+    'no-else-return': ['error', { allowElseIf: false }],
+    'import/extensions': 'warn',
+  },
+};
